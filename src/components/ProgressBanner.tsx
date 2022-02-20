31af47c2c99924e51f436b50854d057a3d70fee2
conache <C.conache@gmail.com>
2022-02-21 00:14:18 +0200

    Display accumulated xyz amount

--- src/components/ProgressBanner.tsx
+++ src/components/ProgressBanner.tsx
@@ -1,29 +1,43 @@
 import React from 'react'
-import { ethers } from 'ethers'
+import { BigNumber, ethers } from 'ethers'
 import usePlarformData from '../hooks/usePlatformData'
 const {
   utils: { formatEther }
 } = ethers
 
+interface ITokenAmount {
+  amount: BigNumber
+  symbol?: string | null
+}
+
+const TokenAmount = ({ amount, symbol }: ITokenAmount) => (
+  <div className="flex">
+    {parseFloat(formatEther(amount)).toFixed(2)}{' '}
+    <div className="ml-1 font-bold text-blue-light">{symbol}</div>
+  </div>
+)
+
 const ProgressBanner = () => {
-  const { mintPrice, tokenSymbol, mintedBricksCount } = usePlarformData()
+  const { mintPrice, tokenSymbol, mintedBricksCount, raisedTokens } = usePlarformData()
 
   return (
-    <div className="flex justify-between w-full bg-blue-dark px-6 py-2">
+    <div className="flex justify-between w-full px-6 py-2 bg-blue-dark">
       <div className="flex">
-        Mint price
-        <div className="font-bold text-blue-light mr-2">(+3%/tx):</div>
-        <div className="flex">
-          {formatEther(mintPrice)}{' '}
-          <div className="text-blue-light ml-1 font-bold">{tokenSymbol}</div>
+        <div className="flex font-bold">
+          Mint price
+          <div className="mr-2 text-blue-light">(+3%/tx):</div>
         </div>
+        <TokenAmount amount={mintPrice} symbol={tokenSymbol} />
+      </div>
+
+      <div className="flex font-bold">
+        <div className="mr-2 font-bold">Minted bricks:</div>
+        <div className="text-blue-light">{mintedBricksCount.toString()}</div>
       </div>
 
       <div className="flex">
-        Minted bricks:
-        <div className="text-blue-light ml-1 font-bold">
-          {mintedBricksCount.toString()}
-        </div>
+        <div className="mr-2 font-bold">Raised XYZ:</div>
+        <TokenAmount amount={raisedTokens} symbol={tokenSymbol} />
       </div>
     </div>
   )
--- src/contexts/PlatformDataContext.tsx
+++ src/contexts/PlatformDataContext.tsx
@@ -10,40 +10,44 @@ interface IPlarformData {
   mintPrice: BigNumber
   mintEnabled: boolean
   mintedBricksCount: number
+  raisedTokens: BigNumber
 }
 
-const PlatformDataContext = React.createContext<IPlarformData>({
+const DEFAULT_VALUES = {
   loading: true,
   tokenName: null,
   tokenSymbol: null,
   mintPrice: BigNumber.from(0),
   mintEnabled: false,
-  mintedBricksCount: 0
-})
+  mintedBricksCount: 0,
+  raisedTokens: BigNumber.from(0)
+}
+
+const PlatformDataContext = React.createContext<IPlarformData>(DEFAULT_VALUES)
 
 const PlatformDataContextProvider: React.FC = ({ children }) => {
-  const [state, setState] = useState<IPlarformData>({
-    loading: true,
-    tokenName: null,
-    tokenSymbol: null,
-    mintPrice: BigNumber.from(0),
-    mintEnabled: false,
-    mintedBricksCount: 0
-  })
+  const [state, setState] = useState<IPlarformData>(DEFAULT_VALUES)
 
   const { blockNumber, refreshCount } = useBlockRefresher()
   const tokenContract = useTokenContract()
   const ledgerContract = useLedgerContract()
 
   const updateData = useCallback(async () => {
-    const [tokenName, tokenSymbol, mintPrice, mintEnabled, mintedBricksCount] =
-      await Promise.all<any>([
-        tokenContract.name(),
-        tokenContract.symbol(),
-        ledgerContract.mintPrice(),
-        ledgerContract.mintEnabled(),
-        ledgerContract.mintedBricksCount()
-      ])
+    const [
+      tokenName,
+      tokenSymbol,
+      mintPrice,
+      mintEnabled,
+      mintedBricksCount,
+      raisedTokens
+    ] = await Promise.all<any>([
+      tokenContract.name(),
+      tokenContract.symbol(),
+      ledgerContract.mintPrice(),
+      ledgerContract.mintEnabled(),
+      ledgerContract.mintedBricksCount(),
+      tokenContract.balanceOf(ledgerContract.address)
+    ])
 
     setState({
       loading: false,
@@ -51,7 +55,8 @@ const PlatformDataContextProvider: React.FC = ({ children }) => {
       tokenSymbol,
       mintPrice,
       mintEnabled,
-      mintedBricksCount: mintedBricksCount.toNumber()
+      mintedBricksCount: mintedBricksCount.toNumber(),
+      raisedTokens
     })
   }, [ledgerContract, tokenContract])
 
